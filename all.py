92ca97d85c322981cfb4092bcc2b3a5a159c20b0
Paul Carpenter <paul.carpenter@bsc.es>
2020-09-28 15:52:50 +0200

    Moved running of experiment to runexperiment.py

--- all.py
+++ all.py
@@ -10,121 +10,6 @@ import topologies
 import runexperiment
 
 
-
-def find_paraver_file():
-	files = os.listdir('.')
-	for f in files:
-		m = re.match('^(.*)\.prv', f)
-		if m:
-			return m.group(1)
-	return None
-		
-
-
-def do_cmd(s):
-	print s
-	sys.stdout.flush()
-	return os.system(s)
-
-def translate(string, d):
-	s = ''
-	for ch in string:
-		s += d.get(ch, ch)
-	return s
-
-def tracedir_name(desc, cmd, policy):
-	cmd2 = translate(cmd, {' ': '_', '/' : '_'})
-	desc2 = translate(desc, {';': '_'})
-	tracedir_opts = runexperiment.params['tracedir_opts']
-
-	return 'trace-%s-%s-%s%s-%s' % (cmd2, desc2, policy, tracedir_opts, str(os.getpid()))
-
-def run_experiment(nodes, deg, desc, cmd):
-	policy = runexperiment.params['policy']
-	extrae_as_threads = runexperiment.params['extrae_as_threads']
-	
-	if policy == 'global':
-		hybrid_policy = 'global'
-		rebalance = True
-	elif policy == 'local':
-		hybrid_policy = 'local'
-		rebalance = False
-	else:
-		assert policy =='no-rebalance'
-		hybrid_policy = 'global' # Global policy
-		rebalance = False # but don't actually rebalance
-
-	print 'Experiment', 'nodes:', nodes, 'deg:', deg, 'desc:', desc, 'cmd:', cmd, policy, 'rebalance:', rebalance
-
-	rebalance_filename = None
-	if rebalance:
-
-		do_cmd('rm -f .kill')
-		do_cmd('rm -rf .hybrid')
-
-		rebalance_filename = 'rebalance-out-%d-%d.txt' % (nodes,deg)
-		rebalance_opts = runexperiment.params['rebalance_opts']
-		do_cmd('${MERCURIUM}/../rebalance/rebalance.py ' + rebalance_opts + '10000 > ' + rebalance_filename + ' &')
-		time.sleep(1)
-
-	if runexperiment.params['instrumentation'] == 'extrae':
-		do_cmd('rm -rf set-0/ TRACE.mpits')
-
-	# Run experiment
-	s = 'NANOS6_CLUSTER_SPLIT="%s" ' % desc
-	s += 'NANOS6_CLUSTER_HYBRID_POLICY="%s" ' % hybrid_policy
-	s += 'MV2_ENABLE_AFFINITY=0 '
-
-	if not runexperiment.params['instrumentation'] is None:
-		s = s + 'NANOS6=%s-debug ' % runexperiment.params['instrumentation']
-	if runexperiment.params['instrumentation']== 'extrae' and runexperiment.params['extrae_preload']:
-	 	s = s + 'EXTRAE_ON=1 '
-
-	if extrae_as_threads:
-		s = s + 'NANOS6_EXTRAE_AS_THREADS=1 '
-	else:
-		if 'NANOS6_EXTRAE_AS_THREADS' in os.environ.keys():
-			del os.environ['NANOS6_EXTRAE_AS_THREADS']
-	if runexperiment.params['local_period'] is not None:
-		s = s + 'NANOS6_LOCAL_TIME_PERIOD=%d ' % runexperiment.params['local_period']
-	else:
-		if 'NANOS6_LOCAL_TIME_PERIOD' in os.environ.keys():
-			del os.environ['NANOS6_LOCAL_TIME_PERIOD']
-	s += 'mpirun -np %d %s ' % (nodes*deg, cmd)
-	retval = do_cmd(s)
-
-	if retval != 0:
-		return retval
-
-	if rebalance:
-		do_cmd('touch .kill')
-	if runexperiment.params['instrumentation'] == 'extrae':
-		# Hack to generate TRACE.mpits file
-		prefix = ''
-		if os.path.exists('create_paraver_trace.py'):
-			prefix = './'
-		elif os.path.exists('../create_paraver_trace.py'):
-			prefix = '../'
-
-		do_cmd(prefix + 'create_paraver_trace.py')
-		do_cmd('mpi2prv -f TRACE.mpits')
-
-		# Now move traces to a subdirectory
-		tracefname = tracedir_name(desc, cmd, policy)
-		tracedir = runexperiment.params['trace_location'] + '/' + tracefname
-		prvroot = find_paraver_file()
-		do_cmd('rm -rf ' + tracedir)
-		do_cmd('mkdir -p ' + tracedir)
-		do_cmd('mv TRACE.mpits set-0 ' + tracedir)
-		do_cmd('mv ' + prvroot + '.prv ' + tracedir + '/' + tracefname + '.prv')
-		do_cmd('mv ' + prvroot + '.pcf ' + tracedir + '/' + tracefname + '.pcf')
-		do_cmd('mv ' + prvroot + '.row ' + tracedir + '/' + tracefname + '.row')
-		do_cmd('mv .hybrid/ ' + tracedir)
-		if not rebalance_filename is None:
-			do_cmd('mv ' + rebalance_filename + ' ' + tracedir)
-
-	return 0
-
 # Options to rebalance.py that are forwarded. The order matters for packing arguments
 #                             Option        has    Short 
 #                                           arg    (for trace)    
@@ -222,7 +107,6 @@ def main(argv):
 			runexperiment.set_param('local_period', int(a))
 		else:
 			try:
-				print '>%s<' % o
 				options = ['--' + name for (name, has_arg, shortname) in rebalance_forwarded_opts]
 				index = options.index(o)
 				name, has_arg, shortname = rebalance_forwarded_opts[index]
@@ -242,23 +126,10 @@ def main(argv):
 
 	assert len(args) >= 2
 	num_nodes = int(args[0])
-	cmd = ' '.join(args[1:])
 
-	if runexperiment.params['extrae_preload'] and runexperiment.params['instrumentation'] == 'extrae':
-		if not 'EXTRAE_HOME' in os.environ:
-			print 'EXTRAE_HOME needs to be set to use --extrae-preload'
-			return 1
-		cmd2 = translate(cmd, {' ': '_', '/' : '_'})
-		extrae_preload_sh = 'preload-%s-%d' % (cmd2, os.getpid())
-		fp = open(extrae_preload_sh, 'w')
-		extrae_library = '%s/lib/libnanosmpitrace.so' % os.environ['EXTRAE_HOME']
-		print >> fp, 'LD_PRELOAD=%s %s' % (extrae_library, cmd)
-		fp.close()
-		do_cmd('cat ' + extrae_preload_sh)
-		cmd = 'sh ' + extrae_preload_sh
+	runexperiment.init(' '.join(args[1:]))
 	
-
-	do_cmd('pwd')
+	runexperiment.do_cmd('pwd')
 
 	for (nodes,deg), desc in sorted(topologies.splits.items()):
 		if nodes == num_nodes:
@@ -290,9 +161,9 @@ def main(argv):
 						runexperiment.set_param('tracedir_opts', tracedir_opts)
 
 						# Clean DLB
-						do_cmd('mpirun -np %d dlb_shm -d' % num_nodes)
+						runexperiment.do_cmd('mpirun -np %d dlb_shm -d' % num_nodes)
 
-						retval = run_experiment(nodes, deg, desc, cmd)
+						retval = runexperiment.run_experiment(nodes, deg, desc)
 						if retval != 0 and (not continue_after_error):
 							return 1
 
@@ -300,8 +171,7 @@ def main(argv):
 						while os.path.exists('.kill'):
 							time.sleep(1)
 
-	if runexperiment.params['extrae_preload'] and runexperiment.params['instrumentation'] == 'extrae':
-		do_cmd('rm ' + extrae_preload_sh)
+	runexperiment.shutdown()
 	return 0
 
 if __name__ == '__main__':
--- runexperiment.py
+++ runexperiment.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import re
+import time
 
 params = {'use_dlb' : True,
           'instrumentation' : None,
@@ -12,3 +16,139 @@ params = {'use_dlb' : True,
 def set_param(name, value):
 	assert name in params.keys()
 	params[name] = value
+
+
+def find_paraver_file():
+	files = os.listdir('.')
+	for f in files:
+		m = re.match('^(.*)\.prv', f)
+		if m:
+			return m.group(1)
+	return None
+
+def do_cmd(s):
+	print s
+	sys.stdout.flush()
+	return os.system(s)
+
+def translate(string, d):
+	s = ''
+	for ch in string:
+		s += d.get(ch, ch)
+	return s
+
+def tracedir_name(desc, cmd, policy):
+	cmd2 = translate(cmd, {' ': '_', '/' : '_'})
+	desc2 = translate(desc, {';': '_'})
+	tracedir_opts = params['tracedir_opts']
+
+	return 'trace-%s-%s-%s%s-%s' % (cmd2, desc2, policy, tracedir_opts, str(os.getpid()))
+
+
+def init(cmd):
+	if params['extrae_preload'] and params['instrumentation'] == 'extrae':
+		if not 'EXTRAE_HOME' in os.environ:
+			print 'EXTRAE_HOME needs to be set to use --extrae-preload'
+			return 1
+		cmd2 = translate(cmd, {' ': '_', '/' : '_'})
+		extrae_preload_sh = 'preload-%s-%d' % (cmd2, os.getpid())
+		fp = open(extrae_preload_sh, 'w')
+		extrae_library = '%s/lib/libnanosmpitrace.so' % os.environ['EXTRAE_HOME']
+		print >> fp, 'LD_PRELOAD=%s %s' % (extrae_library, cmd)
+		fp.close()
+		do_cmd('cat ' + extrae_preload_sh)
+		cmd = 'sh ' + extrae_preload_sh
+	params['cmd'] = cmd
+
+
+def shutdown():
+	if params['extrae_preload'] and params['instrumentation'] == 'extrae':
+		do_cmd('rm ' + extrae_preload_sh)
+
+
+def run_experiment(nodes, deg, desc):
+	cmd = params['cmd']
+	policy = params['policy']
+	extrae_as_threads = params['extrae_as_threads']
+	
+	if policy == 'global':
+		hybrid_policy = 'global'
+		rebalance = True
+	elif policy == 'local':
+		hybrid_policy = 'local'
+		rebalance = False
+	else:
+		assert policy =='no-rebalance'
+		hybrid_policy = 'global' # Global policy
+		rebalance = False # but don't actually rebalance
+
+	print 'Experiment', 'nodes:', nodes, 'deg:', deg, 'desc:', desc, 'cmd:', cmd, policy, 'rebalance:', rebalance
+
+	rebalance_filename = None
+	if rebalance:
+
+		do_cmd('rm -f .kill')
+		do_cmd('rm -rf .hybrid')
+
+		rebalance_filename = 'rebalance-out-%d-%d.txt' % (nodes,deg)
+		rebalance_opts = params['rebalance_opts']
+		do_cmd('${MERCURIUM}/../rebalance/rebalance.py ' + rebalance_opts + '10000 > ' + rebalance_filename + ' &')
+		time.sleep(1)
+
+	if params['instrumentation'] == 'extrae':
+		do_cmd('rm -rf set-0/ TRACE.mpits')
+
+	# Run experiment
+	s = 'NANOS6_CLUSTER_SPLIT="%s" ' % desc
+	s += 'NANOS6_CLUSTER_HYBRID_POLICY="%s" ' % hybrid_policy
+	s += 'MV2_ENABLE_AFFINITY=0 '
+
+	if not params['instrumentation'] is None:
+		s = s + 'NANOS6=%s-debug ' % params['instrumentation']
+	if params['instrumentation']== 'extrae' and params['extrae_preload']:
+	 	s = s + 'EXTRAE_ON=1 '
+
+	if extrae_as_threads:
+		s = s + 'NANOS6_EXTRAE_AS_THREADS=1 '
+	else:
+		if 'NANOS6_EXTRAE_AS_THREADS' in os.environ.keys():
+			del os.environ['NANOS6_EXTRAE_AS_THREADS']
+	if params['local_period'] is not None:
+		s = s + 'NANOS6_LOCAL_TIME_PERIOD=%d ' % params['local_period']
+	else:
+		if 'NANOS6_LOCAL_TIME_PERIOD' in os.environ.keys():
+			del os.environ['NANOS6_LOCAL_TIME_PERIOD']
+	s += 'mpirun -np %d %s ' % (nodes*deg, cmd)
+	retval = do_cmd(s)
+
+	if retval != 0:
+		return retval
+
+	if rebalance:
+		do_cmd('touch .kill')
+	if params['instrumentation'] == 'extrae':
+		# Hack to generate TRACE.mpits file
+		prefix = ''
+		if os.path.exists('create_paraver_trace.py'):
+			prefix = './'
+		elif os.path.exists('../create_paraver_trace.py'):
+			prefix = '../'
+
+		do_cmd(prefix + 'create_paraver_trace.py')
+		do_cmd('mpi2prv -f TRACE.mpits')
+
+		# Now move traces to a subdirectory
+		tracefname = tracedir_name(desc, cmd, policy)
+		tracedir = params['trace_location'] + '/' + tracefname
+		prvroot = find_paraver_file()
+		do_cmd('rm -rf ' + tracedir)
+		do_cmd('mkdir -p ' + tracedir)
+		do_cmd('mv TRACE.mpits set-0 ' + tracedir)
+		do_cmd('mv ' + prvroot + '.prv ' + tracedir + '/' + tracefname + '.prv')
+		do_cmd('mv ' + prvroot + '.pcf ' + tracedir + '/' + tracefname + '.pcf')
+		do_cmd('mv ' + prvroot + '.row ' + tracedir + '/' + tracefname + '.row')
+		do_cmd('mv .hybrid/ ' + tracedir)
+		if not rebalance_filename is None:
+			do_cmd('mv ' + rebalance_filename + ' ' + tracedir)
+
+	return 0
