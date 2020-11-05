0a793a72e85bdcac10ada33f3a85bef0e61443dc
Paul Carpenter <paul.carpenter@bsc.es>
2020-11-05 15:15:55 +0100

    Single degree and single policy per run

--- all.py
+++ all.py
@@ -14,10 +14,11 @@ def Usage():
 	print '.all.py <options>  <num_nodes> <cmd> <args...>'
 	print 'where:'
 	print ' -h                      Show this help'
-	print ' --continue-after-error  To try to find more errors'
 	print ' --vranks                Number of vranks'
-	print ' --min-degree d          Minimum degree'
-	print ' --max-degree d          Maximum degree'
+	print ' --degree d              Set degree'
+	print ' --local                 Use local policy'
+	print ' --global                Use global policy'
+	print ' --no-rebalance          No rebalance policy'
 	print ' --local-period          NANOS6_LOCAL_TIME_PERIOD for local policy'
 	print ' --extrae                Generate extrae trace'
 	print ' --verbose               Generate verbose trace'
@@ -25,6 +26,7 @@ def Usage():
 	print ' --no-extrae-as-threads  Unset NANOS6_EXTRAE_AS_THREADS'
 	print ' --nanos6=               Nanos6 library'
 	print ' --trace-suffix s        Suffix for trace directory and filename'
+	print ' --continue-after-error  To try to find more errors'
 	print 'Options forwarded to rebalance.py:'
 	print '\n'.join([' --%s' % name for (name,has_arg,shortname) in runexperiment.rebalance_forwarded_opts])
 	return 1
@@ -33,13 +35,11 @@ def Usage():
 # Run experiments
 def main(argv):
 
-	min_degree = 1
-	max_degree = 3
+	degree = 2
 	vranks = None
 	continue_after_error = False
-	policies = []
-	threads = []
-
+	policy = None
+	extrae_as_threads = True
 
 	# Sensible initial value
 	runexperiment.set_param('local_period', 150)
@@ -49,8 +49,7 @@ def main(argv):
 		rebalance_getopt = [ name + ('=' if has_arg else '') for (name, has_arg, value) in runexperiment.rebalance_forwarded_opts ]
 
 		opts, args = getopt.getopt( argv[1:],
-									'h', ['help', 'min-per-node=',
-									      'max-per-node=', 'min-degree=', 'max-degree=', 'vranks=',
+									'h', ['help', 'degree=', 'vranks=',
 										  'local', 'global', 'extrae', 'verbose', 'extrae-preload', 'extrae-as-threads',
 										  'no-extrae-as-threads', 'no-rebalance',
 										  'continue-after-error', 'no-dlb',
@@ -63,23 +62,25 @@ def main(argv):
 	for o, a in opts:
 		if o in ('-h', '--help'):
 			return Usage()
-		elif o == '--min-per-node' or o == '--max-per-node':
-			print 'Deprecated --min-per-node and --max-per-node: use --min-degree or --max-degree'
-			return Usage()
-		elif o == '--min-degree':
-			min_degree = int(a)
-		elif o == '--max-degree':
-			max_degree = int(a)
+		elif o == '--degree':
+			degree = int(a)
 		elif o == '--vranks':
 			vranks = int(a)
 		elif o == '--local':
-			if not 'local' in policies:
-				policies.append('local')
+			if not policy is None:
+				print 'Do not specify more than one policy'
+				return 1
+			policy = 'local'
 		elif o == '--global':
-			if not 'global' in policies:
-				policies.append('global')
+			if not policy is None:
+				print 'Do not specify more than one policy'
+				return 1
+			policy = 'global'
 		elif o == '--no-rebalance':
-			policies.append('no-rebalance')
+			if not policy is None:
+				print 'Do not specify more than one policy'
+				return 1
+			policy = 'no-rebalance'
 		elif o == '--extrae':
 			assert runexperiment.params['instrumentation'] is None
 			runexperiment.set_param('instrumentation', 'extrae')
@@ -89,13 +90,11 @@ def main(argv):
 		elif o == '--extrae-preload':
 			runexperiment.set_param('extrae_preload', True)
 		elif o == '--extrae-as-threads':
-			if not True in threads:
-				threads.append(True)
+			extrae_as_threads = True
 		elif o == '--no-extrae-as-threads':
 			print '** --no-extrae-as-threads does work well'
-			sys.exit(1)
-			if not False in threads:
-				threads.append(False)
+			return 1
+			extrae_as_threads = False
 		elif o == '--continue-after-error':
 			continue_after_error = True
 		elif o == '--no-dlb':
@@ -119,11 +118,9 @@ def main(argv):
 			except ValueError:
 				assert False # Should not get here as getopt already checked options were valid
 
-	# Default
-	if threads == []:
-		threads = [True]
-	if policies == []:
-		policies = ['global', 'local']
+	if policy is None:
+		print 'Specify a policy --local, --global or --no-rebalance'
+		return 1
 
 	assert len(args) >= 2
 	nodes = int(args[0])
@@ -132,24 +129,18 @@ def main(argv):
 
 	runexperiment.init(' '.join(args[1:]), rebalance_arg_values)
 	
-	for deg in range(min_degree, max_degree+1):
-		desc = topologies.get_topology(nodes, deg, vranks)
-		if desc is None:
-			continue
+	desc = topologies.get_topology(nodes, degree, vranks)
 
-		for policy in policies:
-			runexperiment.set_param('policy', policy)
-			for extrae_as_threads in threads:
-				runexperiment.set_param('extrae_as_threads', extrae_as_threads)
+	runexperiment.set_param('policy', policy)
+	runexperiment.set_param('extrae_as_threads', extrae_as_threads)
 
+	retval = runexperiment.run_experiment(nodes, degree, vranks, desc)
+	if retval != 0 and (not continue_after_error):
+		return 1
 
-				retval = runexperiment.run_experiment(nodes, deg, vranks, desc)
-				if retval != 0 and (not continue_after_error):
-					return 1
-
-				time.sleep(1)
-				while os.path.exists('.kill'):
-					time.sleep(1)
+	time.sleep(1)
+	while os.path.exists('.kill'):
+		time.sleep(1)
 
 	runexperiment.shutdown()
 	return 0
